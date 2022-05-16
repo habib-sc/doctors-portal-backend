10b2743b4ec1e3e25b28e4fd6e501138753e4db9
habib-sc <habib97.cs@gmail.com>
2022-05-16 08:28:23 +0600

    checking admin or not

--- index.js
+++ index.js
@@ -142,6 +142,13 @@ async function run () {
         }        
       });
 
+      app.get('/admin/:email', async (req, res) => {
+        const email = req.params.email;
+        const user = await usersCollection.findOne({email: email});
+        const isAdmin = user.role === 'admin';
+        res.send({admin: isAdmin});
+      });
+
  
     }
     finally{
