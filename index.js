bdbdf7717deefbd13bc56cb9c04bed2118839f4e
habib-sc <habib97.cs@gmail.com>
2022-05-16 06:47:31 +0600

    receive token form frontend and verify the token for endpoint

--- index.js
+++ index.js
@@ -10,6 +10,23 @@ const jwt = require('jsonwebtoken');
 app.use(cors());
 app.use(express.json());
 
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if(!authHeader){
+    return res.status(401).send({message: 'Unauthorized Access!'});
+  }
+
+  const token = authHeader?.split(' ')[1];
+  jwt.verify(token, process.env.TOKEN_SECRET, (err, decoded) => {
+    if(err){
+      return res.status(403).send({message: 'Forbidden Access!'});
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
 
 // DB Info 
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@doctorsportal.iyeym.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -57,12 +74,19 @@ async function run () {
           res.send(services);
         });
 
-        // Get booking 
-        app.get('/bookings', async (req, res) => {
+        // Get booking by user
+        app.get('/bookings', verifyToken, async (req, res) => {
           const email = req.query.email;
-          const query = {email: email};
-          const bookings = await bookingsCollection.find(query).toArray();
-          res.send(bookings);
+          const decodedEmail = req.decoded.email;
+
+          if (email === decodedEmail) {
+            const query = {email: email};
+            const bookings = await bookingsCollection.find(query).toArray();
+            res.send(bookings);
+          }
+          else{
+            return res.status(403).send({message: 'Forbidden Access'});
+          }
         });
 
 
